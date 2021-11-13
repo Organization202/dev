a2e04d86773181dcab6531bd181ab7647da23323
Organization202 <94239847+Organization202@users.noreply.github.com>
2021-11-13 23:03:54 +0530

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,11 @@
+package javacode;
+
+public class Reverse {
+	public static void main(String[] args) {
+		String name = "Organization", reverse = "";
+		for (int j = name.length() - 1; j >= 0; j--) {
+			reverse = reverse + name.charAt(j);
+		}
+		System.out.println(reverse);
+	}
+}
